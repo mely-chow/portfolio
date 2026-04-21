bbc5beacf94ac0d5e8b787f8d99cef4e79ae483f
mely-chow <melychow@icloud.com>
2026-04-20 18:03:54 -0600

    also fixed spacing in mobile vs larger screen when scrolling

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -37,7 +37,7 @@ function Projects() {
   return (
     <section
       id="projects"
-      className="min-h-screen scroll-mt-24 flex items-start pt-24 md:items-center md:pt-0 relative z-20"
+      className="min-h-screen scroll-mt-24 flex items-start  md:items-center md:pt-0 relative z-20"
     >
       <div className="w-full flex flex-col items-center md:-translate-y-4 lg:-translate-y-6">
         <h2 className="font-display text-2xl">Projects</h2>
